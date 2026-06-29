5ce278a4cc9107c25a2d1e7022bad0c50b2fe437
Joseph Martin <jmkl0987@gmail.com>
2026-06-30 01:29:35 +0530

    Intial layout for the portfolio web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,57 @@
-import Image from "next/image";
+'use client'
+import React, { useRef } from 'react'
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+
+const url = (name: string, wrap = false) =>
+  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 export default function Home() {
+  const parallax = useRef<IParallax>(null!)
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+        <Parallax ref={parallax} pages={3}>
+            <ParallaxLayer
+              offset={0}
+              speed={0.1}
+              style={{
+                display: 'flex',
+                alignItems: 'left',
+                justifyContent: 'left',
+                flexDirection: 'column',
+              }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              <h1 style={{ fontSize: '5rem', color: '#fff', marginLeft: 30, paddingTop: '250px', fontWeight: 700, letterSpacing: '-0.05em' }}>
+                Hi, 
+              </h1>
+              <h1 style={{ fontSize: '5rem', color: '#fff', marginLeft: 30, marginTop: 0, fontWeight: 700, letterSpacing: '-0.05em' }}>I'm Joseph Martin</h1>
+              <h2 style={{ fontSize: '2.5rem', color: '#87BCDE', marginLeft: 30, fontWeight: 300 }}>
+                Full Stack Developer
+              </h2>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={1} speed={1} factor={1} style={
+              { backgroundImage: 'linear-gradient(to bottom, transparent 0%, #805E73 10%, #805E73 20%, #805E73 30%, #805E73 40%, #805E73 50%, #805E73 60%, #805E73 70%, #805E73 80%, #805E73 90%, transparent 100%)' }
+            } />
+            <ParallaxLayer offset={2} speed={1} factor={1} style={
+              { backgroundImage: 'linear-gradient(to bottom, transparent 0%, #87BCDE 10%, #87BCDE 20%, #87BCDE 30%, #87BCDE 40%, #87BCDE 50%, #87BCDE 60%, #87BCDE 70%, #87BCDE 80%, #87BCDE 90%, transparent 100%)' }
+            } />
+
+            <ParallaxLayer
+              offset={0}
+              speed={0}
+              factor={3}
+              style={{
+                backgroundImage: url('stars', true),
+                backgroundSize: 'cover',
+              }}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+            <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+              <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+            </ParallaxLayer>
+
+        </Parallax>
     </div>
+
   );
 }
